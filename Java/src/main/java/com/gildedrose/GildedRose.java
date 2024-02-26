9ff4979db424592855bdaf71e8d131b58948c45a
Cansi Karaca <cansi.karaca@gmail.com>
2024-02-26 02:03:20 +0100

    Move handleDayEndForItem and getStrategyForItem to ItemStrategy.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,17 +1,8 @@
 package com.gildedrose;
 
 import com.gildedrose.strategies.ItemStrategy;
-import com.gildedrose.strategies.impl.AgedBrieStrategy;
-import com.gildedrose.strategies.impl.BackstagePassesStrategy;
-import com.gildedrose.strategies.impl.ConjuredItemStrategy;
-import com.gildedrose.strategies.impl.LegendaryItemStrategy;
-import com.gildedrose.strategies.impl.RegularItemStrategy;
 
 class GildedRose {
-    private static final String SULFURAS = "Sulfuras";
-    private static final String BACKSTAGE_PASSES = "Backstage passes";
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String CONJURED_ITEM = "Conjured";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -21,29 +12,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            handleDayEndForItem(item);
+            ItemStrategy.handleDayEndForItem(item);
         }
-    }
-
-    private static void handleDayEndForItem(Item item) {
-        ItemStrategy itemStrategy = getStrategyForItem(item);
-        itemStrategy.updateItemSellIn(item);
-        itemStrategy.updateItemQuality(item);
-    }
-
-    private static ItemStrategy getStrategyForItem(Item item) {
-        ItemStrategy itemStrategy;
-        if (item.name.equals(AGED_BRIE)) {
-            itemStrategy = new AgedBrieStrategy();
-        } else if (item.name.startsWith(BACKSTAGE_PASSES)) {
-            itemStrategy = new BackstagePassesStrategy();
-        } else if (item.name.startsWith(SULFURAS)) {
-            itemStrategy = new LegendaryItemStrategy();
-        } else if (item.name.startsWith(CONJURED_ITEM)) {
-            itemStrategy = new ConjuredItemStrategy();
-        } else {
-            itemStrategy = new RegularItemStrategy();
-        }
-        return itemStrategy;
     }
 }
--- Java/src/main/java/com/gildedrose/strategies/ItemStrategy.java
+++ Java/src/main/java/com/gildedrose/strategies/ItemStrategy.java
@@ -1,11 +1,21 @@
 package com.gildedrose.strategies;
 
 import com.gildedrose.Item;
+import com.gildedrose.strategies.impl.AgedBrieStrategy;
+import com.gildedrose.strategies.impl.BackstagePassesStrategy;
+import com.gildedrose.strategies.impl.ConjuredItemStrategy;
+import com.gildedrose.strategies.impl.LegendaryItemStrategy;
+import com.gildedrose.strategies.impl.RegularItemStrategy;
 
 public interface ItemStrategy {
     public static final int EXPIRY = 0;
     public static final int MIN_QUALITY = 0;
     public static final int MAX_QUALITY = 50;
+
+    public static final String SULFURAS = "Sulfuras";
+    public static final String BACKSTAGE_PASSES = "Backstage passes";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String CONJURED_ITEM = "Conjured";
 
     default void updateItemSellIn(Item item) {
         item.sellIn--;
@@ -16,5 +26,27 @@ public interface ItemStrategy {
     default boolean isExpired(Item item) {
         final boolean isExpired = item.sellIn < EXPIRY;
         return isExpired;
+    }
+
+    static void handleDayEndForItem(Item item) {
+        ItemStrategy itemStrategy = getStrategyForItem(item);
+        itemStrategy.updateItemSellIn(item);
+        itemStrategy.updateItemQuality(item);
+    }
+
+    static ItemStrategy getStrategyForItem(Item item) {
+        ItemStrategy itemStrategy;
+        if (item.name.equals(AGED_BRIE)) {
+            itemStrategy = new AgedBrieStrategy();
+        } else if (item.name.startsWith(BACKSTAGE_PASSES)) {
+            itemStrategy = new BackstagePassesStrategy();
+        } else if (item.name.startsWith(SULFURAS)) {
+            itemStrategy = new LegendaryItemStrategy();
+        } else if (item.name.startsWith(CONJURED_ITEM)) {
+            itemStrategy = new ConjuredItemStrategy();
+        } else {
+            itemStrategy = new RegularItemStrategy();
+        }
+        return itemStrategy;
     }
 }
